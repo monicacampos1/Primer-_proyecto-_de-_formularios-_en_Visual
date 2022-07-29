ca587f8a1e51ba90e54ef208f1c02897f3e13f74
Monica Campos <USSS005122@ugb.edu.sv>
2022-07-29 11:33:04 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primer__proyecto__de__formularios__en_Visual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
